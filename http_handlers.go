73ae8a5419b71ecd6c29eb887adf47fe692bb037
mrz1836 <mrz1818@gmail.com>
2024-09-16 21:15:29 -0400

    Minor optimizations and linter fixes

--- http_handlers.go
+++ http_handlers.go
@@ -9,10 +9,19 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-// CORSHeaders adds cross origin resource sharing headers to a response
+// Origin is the header key for the origin
+const Origin = "Origin"
+
+// FilteredValue is the value to replace filtered keys with
+const FilteredValue = "FILTERED"
+
+// gZip is the value for gzip
+const gZip = "gzip"
+
+// CORSHeaders adds cross-origin resource sharing headers to a response
 func CORSHeaders(fn http.HandlerFunc) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		if origin := r.Header.Get("Origin"); origin != "" {
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		if origin := r.Header.Get(Origin); origin != "" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
@@ -22,9 +31,9 @@ func CORSHeaders(fn http.HandlerFunc) httprouter.Handle {
 	}
 }
 
-// SendCORS sends a cross origin resource sharing header only
+// SendCORS sends a cross-origin resource sharing header only
 func SendCORS(w http.ResponseWriter, req *http.Request) {
-	if origin := req.Header.Get("Origin"); origin != "" {
+	if origin := req.Header.Get(Origin); origin != "" {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 	}
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
@@ -51,12 +60,14 @@ func GeneralJSONResponse(fn http.HandlerFunc) httprouter.Handle {
 	return EnableGZIP(JSONResp(MakeHTTPRouterParsedReq(CORSHeaders(fn))))
 }
 
-var filterReplace = [...]string{"FILTERED"}
+// filterReplace is the value to replace filtered keys with
+var filterReplace = [...]string{FilteredValue}
 
 // FilteredKeys is a lower case array of keys to filter when logging
 var FilteredKeys []string
 
-// FilterMap will filter the parameters and not log parameters with sensitive data. To add more parameters - see the if in the loop
+// FilterMap will filter the parameters and not log parameters with sensitive data.
+// To add more parameters to filter, add the key to the FilteredKeys array
 func FilterMap(params *Params) *Params {
 	var filtered Params
 	filtered.Values = make(map[string]interface{}, len(params.Values))
@@ -79,7 +90,7 @@ type gzipResponseWriter struct {
 	http.ResponseWriter
 }
 
-// Write write the content
+// Write will write the content
 func (w gzipResponseWriter) Write(b []byte) (int, error) {
 	if "" == w.Header().Get("Content-Type") {
 		// If no content type, apply sniffing algorithm to un-gzipped body.
@@ -91,11 +102,11 @@ func (w gzipResponseWriter) Write(b []byte) (int, error) {
 // EnableGZIP will attempt to compress the response if the client has passed a header value for Accept-Encoding which allows gzip
 func EnableGZIP(fn httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), gZip) {
 			fn(w, r, p)
 			return
 		}
-		w.Header().Set("Content-Encoding", "gzip")
+		w.Header().Set("Content-Encoding", gZip)
 		gz := gzip.NewWriter(w)
 		gzr := gzipResponseWriter{Writer: gz, ResponseWriter: w}
 		fn(gzr, r, p)
